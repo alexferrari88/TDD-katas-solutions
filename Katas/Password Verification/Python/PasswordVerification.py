02aae79832b1c989bb4ad14f803c389c502d4846
alexferrari88 <alessandro.ferrari@imcdgroup.com>
2021-09-06 18:57:26 +0200

    working implementation

---
+++ Katas/Password Verification/Python/PasswordVerification.py
@@ -0,0 +1,54 @@
+﻿import re
+from typing import Optional, List, Tuple
+
+
+class PasswordVerification:
+    class Rule:
+        def __init__(
+            self, value: bool, error_msg: str, critical: Optional[bool] = False
+        ) -> None:
+            self.value = value
+            self.error_msg = error_msg
+            self.critical = critical
+
+    def _has_critical_rule(self, rules: List[Rule]) -> Tuple[bool, str]:
+        for rule in rules:
+            if not rule.value and rule.critical:
+                return True, rule.error_msg
+        return False, None
+
+    def verify(self, pwd: str) -> bool:
+        MIN_RULES_TO_PASS = 3
+        rules = [
+            self.Rule(len(pwd) >= 8, "password should be longer than 8 characters"),
+            self.Rule(
+                bool(re.search(r"[A-Z]", pwd)),
+                "password should have one uppercase letter at least",
+            ),
+            self.Rule(
+                bool(re.search(r"[a-z]", pwd)),
+                "password should have one lowercase letter at least",
+                True,
+            ),
+            self.Rule(
+                bool(re.search(r"\d", pwd)), "password should have one number at least"
+            ),
+        ]
+
+        if not pwd:
+            raise Exception("Error: password should not be null")
+
+        _, error = self._has_critical_rule(rules)
+        if error:
+            raise Exception(error)
+
+        # from https://stackoverflow.com/questions/62840144/if-statement-where-if-3-of-5-conditions-are-true-in-python
+        rules_count = sum(rule.value for rule in rules)
+        if rules_count >= MIN_RULES_TO_PASS:
+            return True
+
+        for rule in rules:
+            if not rule.value:
+                raise Exception(rule.error_msg)
+
+        return True
---
+++ Katas/Password Verification/Python/Test_PasswordVerification.py
@@ -0,0 +1,41 @@
+﻿import pytest
+from .PasswordVerification import PasswordVerification
+
+
+class TestPasswordVerification:
+    password_verification = PasswordVerification()
+
+    def test_should_be_larger_than_8_chars(self):
+        with pytest.raises(Exception) as err:
+            self.password_verification.verify("abc")
+        assert "password should be longer than 8 characters" in str(err.value)
+
+    def test_should_not_be_null(self):
+        with pytest.raises(Exception) as err:
+            self.password_verification.verify("")
+        assert "password should not be null" in str(err.value)
+
+    def test_should_have_at_least_1_uppercase_char(self):
+        with pytest.raises(Exception) as err:
+            self.password_verification.verify("abcdefghi")
+        assert "password should have one uppercase letter at least" in str(err.value)
+
+    def test_should_have_at_least_1_lowercase_char(self):
+        with pytest.raises(Exception) as err:
+            self.password_verification.verify("ABCDEFGHI")
+        assert "password should have one lowercase letter at least" in str(err.value)
+
+    def test_should_have_at_least_1_number(self):
+        with pytest.raises(Exception) as err:
+            self.password_verification.verify("aBcdefghi")
+        assert "password should have one number at least" in str(err.value)
+
+    def test_should_be_ok(self):
+        assert self.password_verification.verify("1aBcdeFgHI")
+        assert self.password_verification.verify("ABcDefGhI")
+        assert self.password_verification.verify("ABc2De")
+        assert self.password_verification.verify("a2bcd3fgh1")
+
+    def test_should_not_pass_without_1_lowercase_char_even_with_3_rules_ok(self):
+        with pytest.raises(Exception) as err:
+            self.password_verification.verify("ABCD3FGH1")
