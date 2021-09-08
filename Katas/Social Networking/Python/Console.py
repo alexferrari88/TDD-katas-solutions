446a170b70bb3841f7704d069e5578b6afcfa894
alexferrari88 <alessandro.ferrari@imcdgroup.com>
2021-09-08 11:44:39 +0200

    created

---
+++ Katas/Social Networking/Python/Console.py
@@ -0,0 +1,9 @@
+﻿from .IOutput import IOutput
+
+
+class Console(IOutput):
+    def write(self, content: str) -> None:
+        print(content)
+
+    def read(self) -> str:
+        return input()
---
+++ Katas/Social Networking/Python/IOutput.py
@@ -0,0 +1,11 @@
+﻿from abc import ABC, abstractmethod
+
+
+class IOutput(ABC):
+    @abstractmethod
+    def write(self, content: str) -> None:
+        """Writes content to output"""
+
+    @abstractmethod
+    def read(self) -> str:
+        """Reads from input"""
